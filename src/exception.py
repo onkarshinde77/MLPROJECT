8ed48468ab21270b9b8a4a861e60faaebb0a56c6
onkarshinde77 <shindeonkar707@gmail.com>
2025-07-25 18:58:01 +0530

    loggin & exception are making

---
+++ src/exception.py
@@ -0,0 +1,29 @@
+'''
+Read documnetation custom exception handling - https://docs.python.org/3/tutorial/errors.html
+
+'''
+
+import sys
+import logging
+
+def error_message(error,error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_mess = "Error occurred in python: \nscript name : [{0}] \nline number : [{1}] \nerror message : [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error)
+    )
+    return error_mess
+
+class CustomException(Exception):
+    def __init__(self,error_mess,error_details:sys):
+        super().__init__(error_mess)
+        self.error_message = error_message(error=error_mess,error_details=error_details)
+    def __str__(self):
+        return self.error_message
+
+
+try:
+    1/0
+except Exception as e:
+    logging.info("Division by zero")
+    raise CustomException(e,sys)
---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m%d%Y%H%M%S')}"  # ex o/p format : 07252025181830
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)    # getcwd() stands for "get current working directory".
+# creating directory contains timestamp
+os.makedirs(logs_path,exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level =logging.INFO,
+)
+
+if __name__=='__main__':
+    logging.info("Logging has starting")
