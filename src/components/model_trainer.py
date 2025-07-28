d978e06fe41ff9487ed923eb6d3d7a0195991e60
onkarshinde77 <shindeonkar707@gmail.com>
2025-07-28 22:40:03 +0530

    train model complete

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,18 +1,17 @@
 import os
 import sys
 
-from sklearn.linear_model import LinearRegression,Ridge,Lasso
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor , GradientBoostingRegressor
-from xgboost import XGBRegressor
-from catboost import CatBoostRegressor
-from sklearn.model_selection import train_test_split
-
 from src.exception import CustomException
 from src.logger import logging
 from dataclasses import dataclass
 from src.utils import save_obj ,evaluate_models , finetune_hyperparameter
+
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
+from xgboost import XGBRegressor
+from catboost import CatBoostRegressor
 
 
 @dataclass
@@ -26,12 +25,7 @@ class ModelTrainer:
     def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info('Split training data and test data')
-            # X_train,X_test,y_train,y_test= train_test_split(
-            #     train_array[:,:-1],
-            #     train_array[:,-1],
-            #     test_array[:,:-1],
-            #     test_array[:,-1],
-            # )
+            
             X_train = train_array[:, :-1]
             y_train = train_array[:, -1]
 
@@ -39,62 +33,110 @@ class ModelTrainer:
             y_test = test_array[:, -1]
 
             params = {
-            		"Linear Regression": {
-                        # LinearRegression rarely needs param tuning; can keep empty or add 'fit_intercept'
-                        'fit_intercept': [True, False]
-                    },
-                    "Lasso": {
-                        'alpha': [0.001, 0.01, 0.1, 1, 10],
-                        'selection': ['cyclic', 'random']
-                    },
-                    "Ridge": {
-                        'alpha': [0.01, 0.1, 1, 10, 100],
-                        'solver': ['svd', 'cholesky', 'lsqr']
-                    },
-                    "K-Neighbors Regressor": {
-                        'n_neighbors': [3, 5, 7, 9],
-                        'weights': ['uniform', 'distance'],
-                        'p': [1, 2]  # p=1: Manhattan, p=2: Euclidean distance
-                    },
-                    "Decision Tree": {
-                        'max_depth': [None, 5, 10, 15],
-                        'min_samples_split': [2, 5, 10],
-                        'min_samples_leaf': [1, 2, 4]
-                    },
-                    "AdaBoost Regressor": {
-                        'n_estimators': [50, 100, 200],
-                        'learning_rate': [0.01, 0.1, 1]
-                    },
-                    "GradientBoosting": {
-                        'n_estimators': [100, 200],
-                        'learning_rate': [0.01, 0.1],
-                        'max_depth': [3, 5, 7]
-                    },
-                    "Random Forest Regressor": {
-                        'n_estimators': [100, 200],
-                        'max_depth': [None, 10, 20],
-                        'min_samples_split': [2, 5],
-                        'min_samples_leaf': [1, 2]
-                    },
-                    "XGBRegressor": {
-                        'n_estimators': [100, 200],
-                        'learning_rate': [0.01, 0.1],
-                        'max_depth': [3, 5, 7]
-                    },
-                    "CatBoosting Regressor": {
-                        'depth': [4, 6, 8],
-                        'learning_rate': [0.01, 0.1],
-                        'iterations': [500, 1000],
-                        'l2_leaf_reg': [1, 3, 5]
+                'LinearRegression': {
+                    'model': LinearRegression(),
+                    'params': {
+                        'fit_intercept': [True, False],
                     }
+                },
+                'Lasso': {
+                    'model': Lasso(max_iter=5000, random_state=42),
+                    'params': {
+                        'alpha': [0.0001, 0.001, 0.01, 0.1, 1, 10],
+                        'selection': ['cyclic', 'random'],
+                        'max_iter': [1000, 5000]
+                    }
+                },
+                'Ridge': {
+                    'model': Ridge(max_iter=5000, random_state=42),
+                    'params': {
+                        'alpha': [0.01, 0.1, 1, 10, 100],
+                        'solver': ['auto', 'svd', 'cholesky', 'lsqr'],
+                        'max_iter': [1000, 5000]
+                    }
+                },
+                'KNeighborsRegressor': {
+                    'model': KNeighborsRegressor(),
+                    'params': {
+                        'n_neighbors': [3, 5, 7, 9, 11],
+                        'weights': ['uniform', 'distance'],
+                        'p': [1, 2],  # 1=Manhattan, 2=Euclidean
+                        'leaf_size': [20, 30, 40]
+                    }
+                },
+                'DecisionTree': {
+                    'model': DecisionTreeRegressor(random_state=42),
+                    'params': {
+                        'criterion': ['squared_error', 'friedman_mse', 'absolute_error'],  # sklearn >=1.0 uses squared_error instead of mse
+                        'max_depth': [None, 5, 10, 20, 50],
+                        'min_samples_split': [2, 5, 10],
+                        'min_samples_leaf': [1, 2, 5],
+                        'max_features': [None,'sqrt', 'log2']
+                    }
+                },
+                'RandomForest': {
+                    'model': RandomForestRegressor(random_state=42),
+                    'params': {
+                        'n_estimators': [100, 200, 300],
+                        'max_depth': [None, 10, 20, 30],
+                        'min_samples_split': [2, 5, 10],
+                        'min_samples_leaf': [1, 2, 4],
+                        'max_features': ['auto', 'sqrt', 'log2']
+                    }
+                },
+                'AdaBoost': {
+                    'model': AdaBoostRegressor(random_state=42),
+                    'params': {
+                        'n_estimators': [50, 100, 200],
+                        'learning_rate': [0.001, 0.01, 0.1, 1],
+                        'loss': ['linear', 'square', 'exponential']
+                    }
+                },
+                'GradientBoosting': {
+                    'model': GradientBoostingRegressor(random_state=42),
+                    'params': {
+                        'n_estimators': [100, 150, 200],
+                        'learning_rate': [0.01, 0.05, 0.1],
+                        'max_depth': [3, 5, 7],
+                        'subsample': [0.6, 0.8, 1.0],
+                        'min_samples_split': [2, 5, 10],
+                        'min_samples_leaf': [1, 2, 4],
+                        'max_features': ['auto', 'sqrt', 'log2']
+                    }
+                },
+                'XGBoost': {
+                    'model': XGBRegressor(random_state=42, verbosity=0, objective='reg:squarederror'),
+                    'params': {
+                        'n_estimators': [100, 200, 300],
+                        'learning_rate': [0.01, 0.05, 0.1],
+                        'max_depth': [3, 5, 7, 9],
+                        'subsample': [0.6, 0.8, 1.0],
+                        'colsample_bytree': [0.6, 0.8, 1.0],
+                        'gamma': [0, 1, 5],
+                        'reg_alpha': [0, 0.01, 0.1],
+                        'reg_lambda': [1, 1.5, 2]
+                    }
+                },
+                'CatBoost': {
+                    'model': CatBoostRegressor(verbose=0, random_state=42),
+                    'params': {
+                        'iterations': [100, 200, 500],
+                        'learning_rate': [0.01, 0.05, 0.1],
+                        'depth': [4, 6, 8, 10],
+                        'l2_leaf_reg': [1, 3, 5, 7, 9],
+                        'border_count': [32, 50, 100],
+                        'thread_count': [4],  # adjust to your CPU cores
+                        'random_strength': [1, 20, 50]
+                    }
+                }
             }
-            
+
             logging.info("Hyperparameter finetune starting!")
             models:dict = finetune_hyperparameter(params=params,X_train=X_train,y_train=y_train)
             logging.info("Hyperparameter finetune completed!")
             model_report : dict = evaluate_models(X_train=X_train,X_test=X_test,
                                                  y_train=y_train,y_test=y_test,models=models)
-            return None
+
             best_model_name = max(model_report,key=model_report.get)
             best_model_score = model_report[best_model_name]
             best_model = models[best_model_name]
@@ -114,4 +156,41 @@ class ModelTrainer:
             raise CustomException(e,sys)
 
 
+# Traceback (most recent call last):
+#   File "D:\Projects\MLPROJECT\src\utils.py", line 29, in evaluate_models
+#     best_model.set_params(**params)
+#   File "D:\Projects\MLPROJECT\venv\Lib\site-packages\catboost\core.py", line 3537, in set_params
+#     raise CatBoostError("You can't change params of fitted model.")
+# _catboost.CatBoostError: You can't change params of fitted model.
 
+# During handling of the above exception, another exception occurred:
+
+# Traceback (most recent call last):
+#   File "D:\Projects\MLPROJECT\src\components\model_trainer.py", line 137, in initiate_model_trainer
+#     model_report : dict = evaluate_models(X_train=X_train,X_test=X_test,
+#                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#   File "D:\Projects\MLPROJECT\src\utils.py", line 39, in evaluate_models
+#     raise CustomException(e,sys)
+# src.exception.CustomException: Error occurred in python:
+# script name : [D:\Projects\MLPROJECT\src\utils.py]
+# line number : [29]
+# error message : [You can't change params of fitted model.]
+
+# During handling of the above exception, another exception occurred:
+
+# Traceback (most recent call last):
+#   File "<frozen runpy>", line 198, in _run_module_as_main
+#   File "<frozen runpy>", line 88, in _run_code
+#   File "D:\Projects\MLPROJECT\src\components\data_ingestion.py", line 55, in <module>
+#     r2_score = model.initiate_model_trainer(train_array=train_arr,test_array=test_arr)
+#                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#   File "D:\Projects\MLPROJECT\src\components\model_trainer.py", line 156, in initiate_model_trainer
+#     raise CustomException(e,sys)
+# src.exception.CustomException: Error occurred in python:
+# script name : [D:\Projects\MLPROJECT\src\components\model_trainer.py]
+# line number : [137]
+# error message : [Error occurred in python:
+# script name : [D:\Projects\MLPROJECT\src\utils.py]
+# line number : [29]
+# error message : [You can't change params of fitted model.]]
+# (venv) PS D:\Projects\MLPROJECT> cls
--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,7 @@ import dill
 
 from sklearn.metrics import r2_score
 from src.exception import CustomException
-from sklearn.model_selection import RandomizedSearchCV
-
+from sklearn.model_selection import RandomizedSearchCV , GridSearchCV
 
 def save_obj(file_path,obj):
     try:
@@ -16,49 +15,71 @@ def save_obj(file_path,obj):
         
         with open(file_path,'wb') as f:
             dill.dump(obj,f)
-        
+            
     except Exception as e:
         raise CustomException(e,sys)
 
 def evaluate_models(X_train,X_test,y_train,y_test,models):
     try:
-        print(models)
-        return None
         report={}
         for key,value in models.items():
             model = value
-            model.fit(X_train,y_train)
-            
+            best_model = model['best_model']
+            params = model['best_params']
+            best_model.set_params(**params)
+            best_model.fit(X_train,y_train)
             # y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            y_test_pred = best_model.predict(X_test)
             # train_score = r2_score(y_train,y_train_pred)
             test_score = r2_score(y_test,y_test_pred)
             report[key] = test_score
         return report
     
-    except Exception as e:
-        CustomException(e,sys)
+    except Exception as e:        
+        raise CustomException(e,sys)
 
 # this method only finetune each algorithm 
-def finetune_hyperparameter(params, X_train, y_train, n_iter=20):
-    best_models = {}
-    for name, config in params.items():
-        print(f"Tuning model: {name}")
-        random_search = RandomizedSearchCV(
-            estimator=config['model'],
-            param_distributions=config['params'],
-            n_iter=n_iter,                    # Number of random combinations to try
-            cv=3,
-            scoring='neg_mean_squared_error',
-            n_jobs=-1,
-            verbose=2,
-            random_state=42
-        )
-        random_search.fit(X_train, y_train)
-        best_models[name] = {
-            'best_model': random_search.best_estimator_,
-            'best_score': -random_search.best_score_,
-            'best_params': random_search.best_params_
-        }
-    return best_models
+def finetune_hyperparameter(params,X_train,y_train):
+    try:
+        best_models = {}
+        for name, config in params.items():
+            grid = GridSearchCV(
+                estimator=config['model'],
+                param_grid=config['params'],
+                cv=3,
+                scoring='neg_mean_squared_error',
+                n_jobs=-1,
+                verbose=2
+            )
+            grid.fit(X_train, y_train)
+            best_models[name] = {
+                'best_model': grid.best_estimator_,
+                'best_score': -grid.best_score_,
+                'best_params': grid.best_params_
+            }
+        return best_models
+    except Exception as e:
+        raise CustomException(e,sys)
+
+# def finetune_hyperparameter(params, X_train, y_train, n_iter=20):
+#     best_models = {}
+#     for name, config in params.items():
+#         print(f"Tuning model: {name}")
+#         random_search = RandomizedSearchCV(
+#             estimator=config['model'],
+#             param_distributions=config['params'],
+#             n_iter=n_iter,                    # Number of random combinations to try
+#             cv=3,
+#             scoring='neg_mean_squared_error',
+#             n_jobs=-1,
+#             verbose=2,
+#             random_state=42
+#         )
+#         random_search.fit(X_train, y_train)
+#         best_models[name] = {
+#             'best_model': random_search.best_estimator_,
+#             'best_score': -random_search.best_score_,
+#             'best_params': random_search.best_params_
+#         }
+#     return best_models
 
